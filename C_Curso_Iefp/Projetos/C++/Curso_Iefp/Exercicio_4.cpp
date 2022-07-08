02f2d802e782db4217339fd9cc0de45ea727c186
Sérgio Miguel Sousa Castro Pereira <tpessoalsp@gmail.com>
2022-07-08 17:20:40 +0100

    .

--- C_Curso_Iefp/Projetos/C++/Curso_Iefp/Exercicio_4.cpp
+++ C_Curso_Iefp/Projetos/C++/Curso_Iefp/Exercicio_4.cpp
@@ -11,5 +11,15 @@ int main()
     soma = x + y;
     cout << "A soma de " << x << " e " << y << " e de: " << soma << "."<< endl;
 
+    int* pntX = &x;
+    int* pntY = &y; // * serve para declarar um ponteiro de um tipo qualquer. & para obter o endereco de memoria de x e y.
+    cout << "O endereco de x e: " << pntX << endl; // endereco de memoria de x.
+    cout << "O endereco de y e: " << pntY << endl; // endereco de memoria de y.
+    cout << "O valor de x e: " << *pntX << endl; // * serve para acessar o valor de x(Diferenciar).
+    cout << "O valor de y e: " << *pntY << endl; // * serve para acessar o valor de y(Diferenciar).
+
+    *pntX=6;
+    cout << "O valor de x e: " << *pntX << endl; // * Alterando o valor do ponteiro de x tambem altera o valor de x.
+    cout << x << endl; // x passa a ter o valor 6.
     return 0;
 }
